77eb84b3e10198e80d45406c0be7294c31c383a8
pairweb <pairweb@adminnoMacBook-Air.local>
2023-10-09 17:15:55 +0900

    change color of hamburger menu

--- js/function.js
+++ js/function.js
@@ -1,6 +1,6 @@
 // close the hamburger menu
 document.addEventListener("DOMContentLoaded", function () {
-  const navContentLinks = document.querySelectorAll("#nav-content li a");
+  const navContentLinks = document.querySelectorAll("#nav-content a li");
 
   navContentLinks.forEach(function (link) {
     link.addEventListener("click", function (event) {
