b7fa951a76ef4183783edebad4d49530bec00bea
hikarugyoji <hikarugyoji@gmail.com>
2024-12-27 01:37:42 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import fs from "fs";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "dist",
   },
-  base: "portfolio",
-  plugins: [react()],
+  base: "/portfolio/",
+  plugins: [
+    react(),
+    {
+      name: "add-nojekyll",
+      closeBundle() {
+        const nojekyllPath = resolve(__dirname, "dist", ".nojekyll");
+        fs.writeFileSync(nojekyllPath, "");
+      },
+    },
+  ],
   optimizeDeps: {
-    exclude: ["lucide-react"],
+    exclude: ["lucide-react"], // 必要に応じてモジュールを除外
   },
 });
