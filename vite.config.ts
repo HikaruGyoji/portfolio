269fa021da91e57bc55f3f8fed5baeca18f2fe51
hikarugyoji <hikarugyoji@gmail.com>
2024-12-27 01:32:32 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   base: "portfolio",
   plugins: [react()],
   optimizeDeps: {
